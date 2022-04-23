7dd70509e3cee2a0a2878403faae27b6693e5466
mzbreach <mzbreach@gmail.com>
2022-04-23 14:35:19 -0500

    Updated catinator.ino

--- catinator.ino
+++ catinator.ino
@@ -108,18 +108,41 @@ Food::~Food(){
 
 }
 
+//Create globally variables/libs
+Food *drinkArr[200];
+Food *foodArr[200];
+
+int buttonYesPIN = D0;
+int buttonNoPIN = D1;
+int buttonAPIN = D2;
+int buttonBPIN = D3;
+int buttonCPIN = D4;
+int buttonTestPIN = D5;
+int buttonYes, buttonNo, buttonA, buttonB, buttonC, buttonTest;
+int LCDPIN1 = A0;
+int LCDPIN2 = A1;
+
+bool foodOrDrink = false; //false for food, true for dink
+bool tempDesired = false; //false for cold, true for hot
+bool desiredCaffeine = false; // false for no, true for yes
+bool desiredSweet = false; //false for no, true for yes
+bool desiredSpice = false; //false for no, true for yes
+bool desiredVegan = false; //false for no, true for yes
+bool desiredVegetarian = false; //false for no, true for yes
+int currTime; //current hour in UTC-6 (centeral standard time)
+
+
 
 void setup(){
-    Food *drinkArr[200];
-    Food *foodArr[200];
-
+    
+    //create food objects (drinks)
     Food hotChocolate("Hot Chocolate", 0.1, true, 4, true, true, false, false, true);
     Food Water("Water", 0.0, false, 4, false, false, false, true, true);
     Food Coffee("Coffee", 0.1, true, 0, true, false, false, true, true);
     Food Tea("Tea", 0.1, true, 4, true, false, false, true, true);
     Food Soda("Soda", 0.0, false, 4, true, true, false, true, true);
 
-
+    //create food objects (food)
     Food EnT("Eggs and Toast", 0.25, true, 0, false, false, false, false, true);
     Food Toast("Toast", 0.1, true, 0, false, false, false, true, true);
     Food Cereal("Cereal", 0.1, false, 0, false, true, false, false, true);
@@ -143,12 +166,12 @@ void setup(){
     Food Cookies("Cookies", 1, true, 3, false, true, false, false, true);
     Food BanSplit("Bannana Split", 0.1, false, 3, false, true, false, false, true);
 
+    //add food objects to pre-created arrays
     drinkArr[0] = &hotChocolate;
     drinkArr[1] = &Water;
     drinkArr[2] = &Coffee;
     drinkArr[3] = &Tea;
     drinkArr[4] = &Soda;
-
     
     foodArr[0] = &EnT;
     foodArr[1] = &Toast;
@@ -173,8 +196,152 @@ void setup(){
     foodArr[20] = &Cookies;
     foodArr[21] = &BanSplit;
 
+    //initialize digital pins
+    pinMode(buttonYesPIN, INPUT);
+    pinMode(buttonNoPIN, INPUT);
+    pinMode(buttonAPIN, INPUT);
+    pinMode(buttonBPIN, INPUT);
+    pinMode(buttonCPIN, INPUT);
+    pinMode(buttonTestPIN, INPUT);
+    pinMode(LCDPIN1, OUTPUT);
+    pinMode(LCDPIN2, OUTPUT);
+
+
+    Time.zone(-6);
+
+}
+
+bool askForFoodOrDrink(){
+    //insert code here to push to the screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // food/hungry
+            return false;
+        }
+        else if(buttonB == 1 && buttonA ==0){// drink/thirsty
+            return true;
+        }
+    }
+}
+
+bool askForDesiredTemp(){
+    //insert code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // hot
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// cold
+            return false;
+        }
+    }
+}
+
+bool askForDesiredCaffeine(){
+    //insert code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // caffeine
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// no caffine
+            return false;
+        }
+    }
+}
+
+bool askForDesiredSweet(){
+    //insert code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // sweet
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// not sweet
+            return false;
+        }
+    }
+}
+
+bool askForDesiredSpice(){
+    //insert code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // spicy
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// no spice
+            return false;
+        }
+    }
+}
+
+bool askForVegan(){
+    //insert code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // vegan
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// not vegan
+            return false;
+        }
+    }
+}
+
+bool askForVegetarian(){
+    //inset code here to push to screen
+
+    while(buttonYes == 0 && buttonNo == 0){
+            buttonA = digitalRead(buttonAPIN);
+            buttonB = digitalRead(buttonBPIN);
+        if(buttonA == 1){ // vegetarian
+            return true;
+        }
+        else if(buttonB == 1 && buttonA ==0){// not vegetarian
+            return false;
+        }
+    }
 }
 
 void loop(){
+    currTime = Time.hour();
+    buttonA = digitalRead(buttonAPIN);
+    buttonB = digitalRead(buttonBPIN);
+    buttonC = digitalRead(buttonCPIN);
+    buttonYes = digitalRead(buttonYesPIN);
+    buttonNo = digitalRead(buttonNoPIN);
+    buttonTest = digitalRead(buttonTestPIN);
+    currTime = Time.hour();
+    
+
+    //ask if hungry or thirsty, return true for drink, false for food
+    foodOrDrink = askForFoodOrDrink();
+    if(foodOrDrink == false){ //if user wants food
+
+
+    }
+    tempDesired = askForDesiredTemp();
+    desiredCaffeine = askForDesiredCaffeine();
+    desiredSweet = askForDesiredSweet();
+    desiredSpice = askForDesiredSpice();
+    desiredVegan = askForVegan();
+    desiredVegetarian = askForVegetarian();
+    
+
+
+
 
 }
