e1643f65dd4469b10c35df26b9b56303b02366f7
mzbreach <mzbreach@gmail.com>
2022-04-25 06:17:35 -0500

    Servo and URL timings tweaked

--- catinator.ino
+++ catinator.ino
@@ -673,7 +673,7 @@ void loop(){
             lcd.print("Your results are in!");
             delay(500);
 
-            for(int i = 0; i < 3; i++) {
+            for(int i = 0; i < 5; i++) {
                 for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
                     {                                  // in steps of 1 degree
                         myservo.write(pos);              // tell servo to go to position in variable 'pos'
