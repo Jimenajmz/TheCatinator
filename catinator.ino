3c29a33bfbc4c07f750a71c034475358939cfb4d
mzbreach <mzbreach@gmail.com>
2022-04-26 17:56:37 -0500

    catinator.ino FINAL

--- catinator.ino
+++ catinator.ino
@@ -675,20 +675,20 @@ void loop(){
             lcd.setCursor(0,0);           
             lcd.print("Your results are in!");
             Particle.publish("recipe_Found", reccomendedItemURL);
-            // delay(500);
+            delay(500);
 
-            // for(int i = 0; i < 5; i++) {
-            //     for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
-            //         {                                  // in steps of 1 degree
-            //             myservo.write(pos);              // tell servo to go to position in variable 'pos'
-            //             delay(10);                       // waits 15ms for the servo to reach the position
-            //         }
-            //         for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
-            //         {
-            //             myservo.write(pos);              // tell servo to go to position in variable 'pos'
-            //             delay(10);                       // waits 15ms for the servo to reach the position
-            //         }     
-            // }      
+            for(int i = 0; i < 5; i++) {
+                for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
+                    {                                  // in steps of 1 degree
+                        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+                        delay(10);                       // waits 15ms for the servo to reach the position
+                    }
+                    for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
+                    {
+                        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+                        delay(10);                       // waits 15ms for the servo to reach the position
+                    }     
+            }      
 
             lcd.clear();
             lcd.setCursor(0,0);           
